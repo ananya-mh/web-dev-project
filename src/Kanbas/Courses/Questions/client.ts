0647a073025adb6f48b16cc749cbf8378cd0de9a
Heta Shah <hetashah@Hetas-MacBook-Air.local>
2024-12-12 13:09:44 -0800

    Updated client for Questions

--- src/Kanbas/Courses/Questions/client.ts
+++ src/Kanbas/Courses/Questions/client.ts
@@ -1,14 +1,15 @@
 import axios from 'axios';
 const REMOTE_SERVER = process.env.REACT_APP_REMOTE_SERVER;
-const QUESTIONS_API = `${REMOTE_SERVER}/api/quizzes`;
+const QUIZZES_API = `${REMOTE_SERVER}/api/quizzes`;
+const QUESTIONS_API = `${REMOTE_SERVER}/api/questions`;
 
-export const fetchQuestion = async (quizId: string, questionId: string) => {
-  const response = await axios.get(`${QUESTIONS_API}/${quizId}/questions/${questionId}`);
+export const fetchQuestion = async (questionId: string) => {
+  const response = await axios.get(`${QUESTIONS_API}/${questionId}`);
   return response.data;
 };
 
 export const fetchQuestionsForQuiz = async (quizId: string) => {
-  const response = await axios.get(`${QUESTIONS_API}/${quizId}/questions`);
+  const response = await axios.get(`${QUIZZES_API}/${quizId}/questions`);
   return response.data;
 };
 
@@ -17,12 +18,12 @@ export const createQuestion = async (quizId: string, question: any) => {
   return response.data;
 };
 
-export const updateQuestion = async (quizId: string, question: any) => {
-  const response = await axios.put(`${QUESTIONS_API}/${quizId}/questions/${question._id}`, question);
+export const updateQuestion = async (question: any) => {
+  const response = await axios.put(`${QUESTIONS_API}/${question._id}`, question);
   return response.data;
 };
 
-export const deleteQuestion = async (quizId: string, questionId: string) => {
-  const response = await axios.delete(`${QUESTIONS_API}/${quizId}/questions/${questionId}`); 
+export const deleteQuestion = async (questionId: string) => {
+  const response = await axios.delete(`${QUESTIONS_API}/questions/${questionId}`); 
   return response.data;
 };
