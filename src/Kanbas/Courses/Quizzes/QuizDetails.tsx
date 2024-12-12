9dbdb4292a96a89dafe5cad501d088561c151d06
Heta Shah <hetashah@Hetas-MacBook-Air.local>
2024-12-12 10:56:52 -0800

    Removed comments from QuizDetails

--- src/Kanbas/Courses/Quizzes/QuizDetails.tsx
+++ src/Kanbas/Courses/Quizzes/QuizDetails.tsx
@@ -11,70 +11,15 @@ export default function QuizDetails () {
   const dispatch = useDispatch();
 
   const fetchQuiz = async () => {
-    // try {
       if (qid !== 'New') {
         const quiz = await client.findQuizByQuizId(qid as string);
         dispatch(setQuiz(quiz));
       }
   };
-//       const fetchedQuizDetails = await quizClient.findQuiz(
-//         cid as string,
-//         qid as string
-//       );
-//       setQuizDetails(fetchedQuizDetails);
-//       const attemptsCheck = await quizClient.checkAttempts(qid as string, currentUser?._id);
-//       setCanAttempt(attemptsCheck.canAttempt);
-//       setUserHasAttempted(attemptsCheck.attempts > 0);
-//       if (fetchedQuizDetails.multipleAttempts) {
-//         setAttemptsLeft(fetchedQuizDetails.attempts - attemptsCheck.attempts);
-//       } else {
-//         setAttemptsLeft(1 - attemptsCheck.attempts);
-//       }
-//       if (fetchedQuizDetails.timeLimit) {
-//         setTimeLeft(fetchedQuizDetails.timeLimit);
-//       }
-//     } catch (error) {
-//       console.error('Error fetching quiz details:', error);
-//     }
-//   };
-
-//   const togglePublish = async () => {
-//     if (!quiz) return;
-
-//     const updatedQuiz = { ...quiz, published: !quiz.published }
-//     try {
-//       await client.updateQuiz(updatedQuiz);
-//       dispatch(setQuizzes([updatedQuiz]));
-//     } catch (err: any) {
-//       setError(err.message);
-//     }
-//   };
 
   useEffect(() => {
     fetchQuiz();
   }, [cid, qid]);
-//   );
-
-//   const formatDateForInput = (dateString: string) => {
-//     try {
-//       const date = new Date(dateString);
-//       const formattedDate = date.toISOString().slice(0, 16);
-//       return formattedDate;
-//     } catch (error) {
-//       console.error('Error formatting date for input:', error);
-//       return dateString;
-//     }
-//   };
-
-//   const formatDate = (dateString: string) => {
-//     try {
-//       const date = new Date(dateString);
-//       return format(date, "MMM d 'at' h a");
-//     } catch (error) {
-//       console.error('Error formatting date:', error);
-//       return dateString;
-//     }
-//   };
 
 const { currentUser } = useSelector((state: any) => state.accountReducer);
 
